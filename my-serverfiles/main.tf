1a9ac52e97a3abccec0bc16ae33a12d427f0842c
jahnavimandarapu <136258864+jahnavimandarapu@users.noreply.github.com>
2023-09-20 17:24:01 +0530

    Created main.tf

---
+++ my-serverfiles/main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name = "ubunkey"
+  vpc_security_groupids = ["sg-0e5437ac2307ed575"]
+  connection {
+  type       = "ssh"
+  user       = "ubuntu"
+  private_key = file("./ubunkey.pem")
+  host      = self.public_ip
+  }
+  provisioner "remote-exec" {
+     inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+  Name = "test-server"
+  }
+  provisioner "local-exec" {
+    command = " echo $(aws_instance.test-server.public_ip) > inventory "
+  }
+  provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/banking-finance-project/my-serverfiles/finance-playbook.yml "
+  }
+}  
+  
+  
