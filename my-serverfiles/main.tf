7326cba9abe84a1cc1c72dbd5a0c165c76e5a7e2
jahnavimandarapu <136258864+jahnavimandarapu@users.noreply.github.com>
2023-09-21 18:29:09 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-08e5424edfe926b43"
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
   key_name = "ubunkey"
   vpc_security_group_ids= ["sg-0e5437ac2307ed575"]
@@ -10,7 +10,7 @@ resource "aws_instance" "test-server" {
   host      = self.public_ip
   }
   provisioner "remote-exec" {
-     inline = [ "echo 'wait to start instance' "]
+     inline = [ " echo 'wait to start instance' "]
   }
   tags = {
   Name = "test-server"
